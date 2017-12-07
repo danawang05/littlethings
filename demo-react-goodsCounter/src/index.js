584103949d2cc77c64da106a3eecaeae8915dd89
danawang <danawang305@gmail.com>
2017-12-07 11:14:21 +0800

    react-goodsCounter
    react-goodsCounter

---
+++ demo-react-goodsCounter/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Counter from './Components/Counter.js';
+import './index.css';
+
+ReactDOM.render(
+  <Counter/>,
+  document.getElementById('root')
+);
